1dd92761d5192a9fb0f12e0027826834b3c63d88
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-04 22:34:58 +0300

    Install react-router-dom

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
+import { CiHome } from "react-icons/ci"
+
 export default function Home() {
     return (
         <main className="absolute w-full flex justify-center min-h-screen overflow-x-hidden bg-mainWhite font-montserrat">
-            {/* Navigation section */}
+            {/* Navigation section - desktop */}
             <section className="w-32 flex-auto flex justify-end" id="app-navigation-container">
                 <nav className="block w-64 p-5">
-                    <h1 className="text-3xl font-bold">Zy<span className="text-mainGreen">n</span>kle</h1>
+                    <h1 className="text-4xl font-bold mb-10">Zy<span className="text-mainGreen">n</span>kle</h1>
 
-                    <a>
-                        
+                    <a className="flex align-middle text-xl mb-4 p-2 cursor-pointer">
+                        <CiHome className="my-auto"/>
+                        <p className="my-auto ms-2">Home</p>
+                    </a>
+                    <a className="flex align-middle text-xl p-2 cursor-pointer">
+                        <CiHome className="my-auto"/>
+                        <p className="my-auto ms-2">Home</p>
                     </a>
                 </nav>
             </section>
