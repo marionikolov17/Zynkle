80618352f689f0115ed21554de7d5e39928c2e9a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-14 02:02:53 +0300

    Create Mobile Navigation layout

--- client/src/features/home/components/MobileNavigation/MobileNavigation.jsx
+++ client/src/features/home/components/MobileNavigation/MobileNavigation.jsx
@@ -1,6 +1,6 @@
 export default function MobileNavigation() {
     return (
-        <div className="sm:hidden">
+        <div className="flex w-full fixed bottom-0 bg-mainWhite border-t-2 h-10 z-50 sm:hidden">
 
         </div>
     )
--- client/src/features/home/components/NoPosts/NoPosts.jsx
+++ client/src/features/home/components/NoPosts/NoPosts.jsx
@@ -1,6 +1,6 @@
 export default function NoPosts() {
     return (
-        <div className="w-full flex justify-center mt-3 mb-5">
+        <div className="w-full flex justify-center mt-3 mb-8 sm:mb-5">
             <div className="w-[50%] grow p-4 flex flex-col items-center justify-center">
                 <img className="w-24 sm:w-32" src="./images/no-post-panda.png" alt="" />
                 <p className="mt-2 font-bold text-sm sm:text-lg text-center">Oops... There are no more posts, yet.</p>
