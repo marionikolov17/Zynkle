e230700d91432e7cf6078e8153f334b7423f7288
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-14 00:18:01 +0300

    Fix columns scrolling in Home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import Post from "../../features/home/components/Post/Post"
 
 export default function Home() {
     return (
-        <main className="absolute w-full flex justify-center min-h-screen overflow-x-hidden bg-mainWhite font-montserrat">
+        <main className="absolute w-full h-full flex justify-center min-h-screen overflow-x-hidden bg-mainWhite font-montserrat">
             {/* Navigation section - desktop */}
-            <section className="flex-auto grow-0 flex justify-end border-e-2 border-slate-200" id="app-navigation-container">
+            <section className="flex-auto grow-0 flex justify-end border-e-2 border-slate-200 sticky top-0">
                 <nav className="block w-64 p-8">
                     <h1 className="text-4xl font-bold mb-10">Zy<span className="text-mainGreen">n</span>kle</h1>
 
@@ -25,7 +25,7 @@ export default function Home() {
                 </nav>
             </section>
             {/* Content section */}
-            <section className="max-h-full flex-auto grow border-e-2 border-slate-200 block overflow-x-hidden overflow-y-scroll no-scrollbar">
+            <section className="max-h-full min-h-full flex-auto grow border-e-2 border-slate-200 block overflow-x-hidden overflow-y-scroll no-scrollbar">
                 <header className="flex justify-center py-10">
                     <button className="flex text-xl text-mainGreen py-2 px-10 border-mainGreen border-2 rounded-lg">
                         <CiCirclePlus className="my-auto"/>
@@ -37,7 +37,7 @@ export default function Home() {
                     <Post />
                 </div>
             </section>
-            <section className="flex-auto shrink grow-0 w-[25%]">
+            <section className="flex-auto shrink grow-0 w-[25%] sticky top-0">
 
             </section>
         </main>
