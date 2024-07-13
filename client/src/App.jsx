b0fc07e79d2e64fe8180db6b12bef8ea99ec4028
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-14 01:41:55 +0300

    Fix BrowserRouter placing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Home />
-    </>
+    </BrowserRouter>
   )
 }
 
