a3b065b8ea430e743c1bd98887977487cbd466f3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-14 18:54:51 +0300

    Create Mobile Navigation

--- client/src/features/home/components/MobileNavigation/MobileNavigation.jsx
+++ client/src/features/home/components/MobileNavigation/MobileNavigation.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import { CiHome, CiSearch, CiCirclePlus } from "react-icons/ci";
+import ProfilePicture from "./../../../../components/ProfilePicture/ProfilePicture"
+
 export default function MobileNavigation() {
     return (
-        <div className="flex w-full fixed bottom-0 bg-mainWhite border-t-2 h-10 z-50 sm:hidden">
-
+        <div className="flex items-center justify-around w-full fixed bottom-0 bg-mainWhite border-t-2 h-10 z-50 sm:hidden">
+            <Link>
+                <CiHome className="text-2xl my-auto"/>
+            </Link>
+            <Link>
+                <CiSearch className="text-2xl my-auto"/>
+            </Link>
+            <Link>
+                <CiCirclePlus className="text-2xl my-auto"/>
+            </Link>
+            <Link>
+                <ProfilePicture className="w-6 h-6"/>
+            </Link>
         </div>
     )
 }
