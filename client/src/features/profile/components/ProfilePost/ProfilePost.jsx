2392b96e3da2a1ab245f4fa705f816b5e4f1d1c9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-15 23:24:06 +0300

    Create Profile Page

---
+++ client/src/features/profile/components/ProfilePost/ProfilePost.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function ProfilePost() {
+  return (
+    <Link className="row-span-3 flex">
+      <img
+        className="object-cover"
+        src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"
+        alt=""
+      />
+    </Link>
+  );
+}
