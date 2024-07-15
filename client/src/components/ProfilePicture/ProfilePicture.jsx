173ba15a861b1745699a60760595872fd4657a2c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-15 23:35:47 +0300

    Update ProfilePicture.jsx

--- client/src/components/ProfilePicture/ProfilePicture.jsx
+++ client/src/components/ProfilePicture/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 export default function ProfilePicture({ className }) {
   return (
-    <Link className={className + " " + "flex justify-center align-middle overflow-hidden rounded-full"}>
+    <Link to="/profile" className={className + " " + "flex justify-center align-middle overflow-hidden rounded-full"}>
       <img
         className="object-cover"
         src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
