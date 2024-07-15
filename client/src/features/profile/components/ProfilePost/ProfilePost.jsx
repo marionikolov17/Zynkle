15bb8acc241ed5c5ded807c30b36272e328ddc5f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-16 00:39:48 +0300

    Update ProfilePost.jsx

--- client/src/features/profile/components/ProfilePost/ProfilePost.jsx
+++ client/src/features/profile/components/ProfilePost/ProfilePost.jsx
@@ -4,7 +4,7 @@ export default function ProfilePost() {
   return (
     <Link className="row-span-3 flex">
       <img
-        className="object-cover"
+        className="object-cover hover:brightness-75 transition"
         src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"
         alt=""
       />
