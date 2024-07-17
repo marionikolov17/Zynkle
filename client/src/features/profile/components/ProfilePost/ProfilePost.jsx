2e153b7c10da0bb8b5d671519b1995874562b618
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-17 20:20:20 +0300

    Update ProfilePost.jsx

--- client/src/features/profile/components/ProfilePost/ProfilePost.jsx
+++ client/src/features/profile/components/ProfilePost/ProfilePost.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ProfilePost() {
   return (
-    <Link className="row-span-3 flex">
+    <Link to="/post/1" className="row-span-3 flex">
       <img
         className="object-cover hover:brightness-75 transition"
         src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"
