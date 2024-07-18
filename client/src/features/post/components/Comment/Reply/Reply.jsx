a1024b747e4374fcca33b3ab88e4ba855370b0a6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-18 14:19:27 +0300

    Update Reply.jsx

--- client/src/features/post/components/Comment/Reply/Reply.jsx
+++ client/src/features/post/components/Comment/Reply/Reply.jsx
@@ -6,7 +6,7 @@ export default function Reply() {
     <div className="flex w-full ps-6 sm:ps-12 my-4">
       <div className="ps-6">
         {" "}
-        {/* Comment Owner Image */}
+        {/* Reply Owner Image */}
         <ProfilePicture className="w-10 h-10" />
       </div>
       <div className="block grow ms-3">
@@ -14,15 +14,14 @@ export default function Reply() {
         <h3 className="text-sm font-bold">
           <span className="text-mainGreen">@</span>ivantodorov
         </h3>
-        {/* Comment text content */}
+        {/* Reply text content */}
         <p className="text-[13px] sm:text-sm">
           This is an awesome coment and I want to read it out loud!!
         </p>
-        {/* Comment buttons */}
+        {/* Reply buttons */}
         <div className="flex mt-2">
           <p className="text-xs lg:text-sm opacity-70">2 days</p>
           <p className="text-xs lg:text-sm ms-4 cursor-pointer">3 likes</p>
-          <button className="text-xs lg:text-sm ms-4">Reply</button>
         </div>
       </div>
       <div className="p-3">
