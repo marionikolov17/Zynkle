005bf223c6a4a8f994572d5959f280c628ba4df0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-18 13:56:23 +0300

    Refactor Post page to desktop and mobile

---
+++ client/src/pages/Post/MobilePost/MobilePost.jsx
@@ -0,0 +1,7 @@
+export default function MobilePost() {
+    return (
+        <main className="sm:hidden">
+
+        </main>
+    )
+}
--- client/src/pages/Post/Post.jsx
+++ client/src/pages/Post/Post.jsx
@@ -1,42 +1,11 @@
-import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
-import AddCommentForm from "../../features/post/components/AddCommentForm/AddCommentForm";
-import Comment from "../../features/post/components/Comment/Comment";
-import PostStats from "../../features/post/components/PostStats/PostStats";
+import DesktopPost from "./DesktopPost/DesktopPost";
+import MobilePost from "./MobilePost/MobilePost";
 
 export default function Post() {
   return (
-    <main className="absolute overflow-x-hidden flex justify-center items-center min-h-screen max-h-max w-full bg-mainWhite font-montserrat">
-      <div className="grow xl:grow-0 xl:w-[75%] 2xl:w-[60%] min-h-full max-h-max">
-        <div className="bg-white w-full h-[700px] flex">
-          {/* Post Image */}
-          <div className="flex grow-0 shrink">
-            <img
-              className="object-cover"
-              src="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"
-              alt=""
-            />
-          </div>
-          {/* Post details and comments */}
-          <div className="grow flex flex-col w-[95%] lg:w-[65%]">
-            {/* Owner info */}
-            <div className="flex items-center py-3 px-6 border-b">
-              <ProfilePicture className="w-10 h-10" />
-              <h3 className="text-sm ms-3 font-bold">
-                <span className="text-mainGreen">@</span>marionikolov17
-              </h3>
-            </div>
-            {/* Comments and Post Description */}
-            <div className="block grow overflow-y-scroll no-scrollbar border-b">
-                <Comment />
-                <Comment />
-            </div>
-            {/* Post Stats */}
-            <PostStats />
-            {/* Add comment form */}
-            <AddCommentForm />
-          </div>
-        </div>
-      </div>
-    </main>
+    <>
+        <DesktopPost />
+        <MobilePost />
+    </>
   );
 }
