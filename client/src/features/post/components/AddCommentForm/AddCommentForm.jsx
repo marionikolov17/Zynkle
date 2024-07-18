9a651b5432a6f6c15abfb3bc5b9505a59596a1a6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-18 12:53:15 +0300

    Update AddCommentForm.jsx

--- client/src/features/post/components/AddCommentForm/AddCommentForm.jsx
+++ client/src/features/post/components/AddCommentForm/AddCommentForm.jsx
@@ -3,13 +3,13 @@ export default function AddCommentForm() {
     <form className="w-full flex px-4 py-3">
       <input
         type="text"
-        className="grow max-w-full outline-none"
+        className="grow max-w-full outline-none text-sm"
         name="commentInput"
         id="commentInput"
         placeholder="Add comment..."
         required
       />
-      <button className="grow-0 shrink max-w-max text-mainGreen">
+      <button className="grow-0 shrink max-w-max text-sm text-mainGreen">
         Comment
       </button>
     </form>
