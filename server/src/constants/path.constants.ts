9b61b0d8a1b88eca15b137569522574824cf47f7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 22:35:31 +0300

    Update path.constants.ts

--- server/src/constants/path.constants.ts
+++ server/src/constants/path.constants.ts
@@ -2,6 +2,7 @@ const PATH = {
     USERS: {
         REGISTER: "/register",
         LOGIN: "/login",
+        GET_TOP_CREATORS: "/top-creators",
         GET_USER: "/:userId",
         FOLLOW_USER: "/:userId/follow",
         UNFOLLOW_USER: "/:userId/unfollow"
