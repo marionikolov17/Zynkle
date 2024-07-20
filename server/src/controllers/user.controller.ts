ea46f566d4425ba47fbe85df013d8d397724da32
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 21:29:46 +0300

    Implement register route in user controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,5 +1,32 @@
 import express from "express";
+import * as authService from "./../services/auth.service";
 
 const router = express.Router();
+
+router.post(
+  "/register",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const [accessToken, refreshToken, session] =
+        await authService.registerUser(req.body);
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          session,
+          accessToken,
+          refreshToken,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: "fail",
+        data: {
+          message: err.message,
+        },
+      });
+    }
+  }
+);
 
 export default router;
