a706bd147b46aa227b1ee3169afc6835f72bb7c7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 23:02:01 +0300

    Update Post model

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -1,46 +1,40 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    creator: {
-        type: mongoose.Types.ObjectId,
-        ref: "User"
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUri: {
+    type: String,
+    required: true,
+  },
+  likedBy: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
-    description: {
-        type: String,
-        required: true
+  ],
+  savedBy: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
-    imageUri: {
-        type: String,
-        required: true
+  ],
+  comments: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment",
     },
-    likedBy: [
-        {
-            user: {
-                type: mongoose.Types.ObjectId,
-                ref: "User"
-            }
-        }
-    ],
-    savedBy: [
-        {
-            user: {
-                type: mongoose.Types.ObjectId,
-                ref: "User"
-            }
-        }
-    ],
-    comments: [
-        {
-            comment: {
-                type: mongoose.Types.ObjectId,
-                ref: "Comment"
-            }
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const postModel = mongoose.model("Post", postSchema);
