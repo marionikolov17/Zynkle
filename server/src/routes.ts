7fb0cea1e3dc73cea03b3dd13e39077623d8e9c8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 20:29:04 +0300

    Import user controller in main router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import userController from "./controllers/user.controller";
 
 const router = express.Router();
+
+router.use("/users", userController);
 
 export default router;
