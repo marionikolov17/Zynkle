77613dfff89b9089c994bc03915eea2d3f7faead
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 20:22:52 +0300

    Update auth.middleware.ts

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import * as jwt from "./../lib/jwt.lib";
 
 export const authMiddleware = async (
