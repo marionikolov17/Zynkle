cd001391a5b158d402ae75e1754bd13af44b6118
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 20:22:31 +0300

    Create auth entry middleware and isAuth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "./../lib/jwt.lib";
+
+export const authMiddleware = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.get("Authorization");
+
+  if (!token) return next();
+
+  try {
+    const decoded = await jwt.verify(token, process.env.SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch {
+    next();
+  }
+};
+
+export const isAuth = (req: any, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    return res
+      .status(401)
+      .json({ status: "fail", data: { message: "You must login!" } });
+  }
+
+  next();
+};
