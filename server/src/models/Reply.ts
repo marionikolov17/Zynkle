d6b7fe363dc361e9248186b69b7a4edae822a76e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 23:09:35 +0300

    Create Reply model

---
+++ server/src/models/Reply.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const replySchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Post"
+    },
+    commentId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Comment"
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likedBy: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const replyModel = mongoose.model("Reply", replySchema);
+export default replyModel;
