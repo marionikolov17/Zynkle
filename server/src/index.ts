ee4c4325e40db7a0e23ca5e43654a07f3ea91d1c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 20:12:39 +0300

    Use cors to allowing access to FE

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,18 +6,25 @@ import cors from "cors";
 import expressConfig from "./config/express.config";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 expressConfig(app);
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost:27017/", { dbName: process.env.DB_NAME })
-        .then(() => {
-            console.log("DB Connected successfully!");
-            app.listen(PORT, () => {
-                console.log(`Server is listening on port ${PORT}`);
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+mongoose
+  .connect("mongodb://localhost:27017/", { dbName: process.env.DB_NAME })
+  .then(() => {
+    console.log("DB Connected successfully!");
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
