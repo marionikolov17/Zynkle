dbd8db034e97d902182ffd155cb9ce0f6eb5267f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 21:39:43 +0300

    Update User model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -16,6 +16,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
     description: {
         type: String,
     },
