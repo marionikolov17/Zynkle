2dddc0fd58e27df5f41e5911b9474bbef0ae23b9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 20:11:49 +0300

    Set up mongoDB and connect it

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import expressConfig from "./config/express.config";
 
 const app = express();
 
+expressConfig(app);
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+mongoose.connect("mongodb://localhost:27017/", { dbName: process.env.DB_NAME })
+        .then(() => {
+            console.log("DB Connected successfully!");
+            app.listen(PORT, () => {
+                console.log(`Server is listening on port ${PORT}`);
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
