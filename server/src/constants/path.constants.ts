f87c4a015b31abf73cffd8cecf837fa493c04deb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 22:34:22 +0300

    Update path.constants.ts

--- server/src/constants/path.constants.ts
+++ server/src/constants/path.constants.ts
@@ -1,8 +1,12 @@
 const PATH = {
     USERS: {
         REGISTER: "/register",
-        LOGIN: "/login"
-    }
+        LOGIN: "/login",
+        GET_USER: "/:userId",
+        FOLLOW_USER: "/:userId/follow",
+        UNFOLLOW_USER: "/:userId/unfollow"
+    },
+    POSTS: {}
 }
 
 export default PATH;
