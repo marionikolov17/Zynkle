e59049e7a1e1f5074425afe4ed94fab96866cdd2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-20 23:08:14 +0300

    Create Comment model

---
+++ server/src/models/Comment.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Post"
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likedBy: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    replies: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Reply"
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const commentModel = mongoose.model("Comment", commentSchema);
+export default commentModel;
