81ec6a03d076538530fa002dd676c5f5a0c23eac
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-21 18:21:51 +0300

    Allow only JPG images to be uploaded

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -1,7 +1,18 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage, limits: { fileSize: 2000000 } });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2000000 },
+  fileFilter: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    if (ext !== ".jpg") {
+        return cb(new Error("Only JPG images are allowed"));
+    }
+    cb(null, true);
+  },
+});
 
 export default upload;
