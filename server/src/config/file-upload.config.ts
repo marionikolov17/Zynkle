d2b210d8e8383a56db01be47774d206ccb3f333f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-21 18:14:54 +0300

    Update file upload size limit to 2MB

--- server/src/config/file-upload.config.ts
+++ server/src/config/file-upload.config.ts
@@ -2,6 +2,6 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage: storage, limits: { fileSize: 10000000 } });
+const upload = multer({ storage: storage, limits: { fileSize: 2000000 } });
 
 export default upload;
