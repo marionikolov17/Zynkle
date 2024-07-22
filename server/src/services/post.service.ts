7e4584d43877776c28246998b922ca6b94e7b2c6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 12:34:02 +0300

    Implement create post method in post service

--- server/src/services/post.service.ts
+++ server/src/services/post.service.ts
@@ -1,10 +1,17 @@
 import { Types } from "mongoose";
 import postModel from "./../models/Post";
+import userModel from "./../models/User";
+
+import { uploadFileToCloud } from "./../utils/storage-upload";
 
 export const createPost = async (
   data: Record<string, any>,
   userId: Types.ObjectId,
   file: Express.Multer.File
 ) => {
-    
+    const imageUri = await uploadFileToCloud(file);
+
+    const createdPost = await postModel.create({ ...data, creator: userId, imageUri: imageUri });
+
+    await userModel.findOneAndUpdate({ _id: userId }, { posts: { $push: createdPost._id } });
 };
