9767b1b06ec75e334968e6769eca8de71cb062d5
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:20:27 +0300

    Import replies controller in main app router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import userController from "./controllers/user.controller";
 import postController from "./controllers/post.controller";
 import commentController from "./controllers/comment.controller";
+import replyController from "./controllers/reply.controller";
 
 const router = express.Router();
 
 router.use("/users", userController);
 router.use("/posts", postController);
 router.use("/comments", commentController);
+router.use("/replies", replyController);
 
 export default router;
