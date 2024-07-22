f20d35fb49d5cc14409a723bed0634a83ed8e0fc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 12:30:43 +0300

    Create route for creating posts

--- server/src/controllers/post.controller.ts
+++ server/src/controllers/post.controller.ts
@@ -1,13 +1,31 @@
-import { tryCatch } from "./../utils/tryCatch";
-import PATH from "./../constants/path.constants";
 import express from "express";
+
+import * as postService from "./../services/post.service";
+
+import RESPONSE_STATUS from "./../constants/response-statuses.constants";
+import PATH from "./../constants/path.constants";
+import upload from "./../config/file-upload.config";
+
+import { isAuth } from "./../middlewares/auth.middleware";
+import { fileTypeValidationMiddleware } from "./../middlewares/validation.middleware";
+import { tryCatch } from "./../utils/tryCatch";
 
 const router = express.Router();
 
 router.post(
     PATH.POSTS.CREATE_POST,
-    tryCatch(async (req: express.Request, res: express.Response) => {
-        
+    isAuth,
+    upload.single("imageUri"),
+    fileTypeValidationMiddleware,
+    tryCatch(async (req: any, res: express.Response) => {
+        await postService.createPost(req.body, req.user._id, req.file);
+
+        res.status(200).json({
+            status: RESPONSE_STATUS.SUCCESS,
+            data: {
+              message: "Successfully created post"
+            }
+          })
     })
 )
 
