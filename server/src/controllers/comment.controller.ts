19a3edf94b6951f94b5888ec2f7280fdb5e7e1dc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:28:20 +0300

    Implement route for creating comments

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -1,5 +1,28 @@
 import express from "express";
 
+import * as commentService from "./../services/comment.service";
+
+import PATH from "./../constants/path.constants";
+import RESPONSE_STATUS from "./../constants/response-statuses.constants";
+
+import { isAuth } from "./../middlewares/auth.middleware";
+import { tryCatch } from "./../utils/tryCatch";
+
 const router = express.Router();
+
+router.post(
+    PATH.COMMENTS.CREATE_COMMENT,
+    isAuth,
+    tryCatch(async (req: any, res: express.Response) => {
+        await commentService.createComment(req.body, req.params.postId, req.user._id);
+
+        res.status(201).json({
+            status: RESPONSE_STATUS.SUCCESS,
+            data: {
+                message: "Successfully created comment"
+            }
+        });
+    })
+);
 
 export default router;
