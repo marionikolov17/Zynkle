5adb4399627da2f7649401b4a84d7f50c2db697a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 12:19:42 +0300

    Create all path constants for all routes

--- server/src/constants/path.constants.ts
+++ server/src/constants/path.constants.ts
@@ -9,7 +9,29 @@ const PATH = {
         UNFOLLOW_USER: "/:userId/unfollow",
         SEARCH_USERS: "/search"
     },
-    POSTS: {}
+    POSTS: {
+        GET_POSTS: "/",
+        CREATE_POST: "/",
+        DELETE_POST: "/:postId",
+        LIKE_POST: "/:postId/like",
+        DISLIKE_POST: "/:postId/dislike",
+        SAVE_POST: "/:postId/save",
+        UNSAVE_POST: "/:postId/unsave"
+    },
+    COMMENTS: {
+        CREATE_COMMENT: "/:postId",
+        GET_COMMENTS: "/:postId",
+        DELETE_COMMENT: "/:commentId",
+        LIKE_COMMENT: "/:commentId/like",
+        DISLIKE_COMMENT: "/:commentId/dislike"
+    },
+    REPLIES: {
+        CREATE_REPLY: "/:postId/:commentId",
+        GET_REPLIES: "/:postId/:commentId",
+        DELETE_REPLY: "/:replyId",
+        LIKE_REPLY: "/:replyId/like",
+        DISLIKE_REPLY: "/:replyId/dislike"
+    }
 }
 
 export default PATH;
