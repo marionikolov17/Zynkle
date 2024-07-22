9238d8be33051c3e89d08ad0775cd33aa963a189
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:43:08 +0300

    Implement create reply route

--- server/src/controllers/reply.controller.ts
+++ server/src/controllers/reply.controller.ts
@@ -1,5 +1,28 @@
 import express from "express";
 
+import * as replyService from "./../services/reply.service";
+
+import PATH from "./../constants/path.constants";
+import RESPONSE_STATUS from "./../constants/response-statuses.constants";
+
+import { isAuth } from "./../middlewares/auth.middleware";
+import { tryCatch } from "./../utils/tryCatch";
+
 const router = express.Router();
+
+router.post(
+    PATH.REPLIES.CREATE_REPLY,
+    isAuth,
+    tryCatch(async (req: any, res: express.Response) => {
+        await replyService.createReply(req.body, req.params.commentId, req.user._id);
+
+        res.status(201).json({
+            status: RESPONSE_STATUS,
+            data: {
+                message: "Successfully created reply"
+            }
+        })
+    })
+);
 
 export default router;
