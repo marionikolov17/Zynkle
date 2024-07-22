78c86b6778a4ae3e4142383a62b1836df90a7814
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 15:52:08 +0300

    Create params.middleware.ts

---
+++ server/src/middlewares/params.middleware.ts
@@ -0,0 +1,58 @@
+import { NextFunction, Request, Response } from "express";
+
+import * as postService from "./../services/post.service";
+import * as commentService from "./../services/comment.service";
+import * as replyService from "./../services/reply.service";
+
+import RESPONSE_STATUS from "./../constants/response-statuses.constants";
+
+export const checkPostId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  value: any
+) => {
+  if (!(await postService.checkIfPostExsists(value))) {
+    return res.status(404).json({
+      status: RESPONSE_STATUS.FAILED,
+      data: {
+        message: "Post not found",
+      },
+    });
+  }
+  next();
+};
+
+export const checkCommentId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  value: any
+) => {
+  if (!(await commentService.checkIfCommentExsists(value))) {
+    return res.status(404).json({
+      status: RESPONSE_STATUS.FAILED,
+      data: {
+        message: "Comment not found",
+      },
+    });
+  }
+  next();
+};
+
+export const checkReplyId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  value: any
+) => {
+  if (!(await replyService.checkIfReplyExsists(value))) {
+    return res.status(404).json({
+      status: RESPONSE_STATUS.FAILED,
+      data: {
+        message: "Reply not found",
+      },
+    });
+  }
+  next();
+};
