9b87bf5f74cdafe7405089b35cc8ed074ca65ad7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:40:13 +0300

    Implement createReply method in reply service

--- server/src/services/reply.service.ts
+++ server/src/services/reply.service.ts
@@ -1,6 +1,12 @@
+import { Types } from "mongoose";
 import replyModel from "./../models/Reply";
 import commentModel from "./../models/Comment";
 
 export const createReply = async (
     data: Record<string, any>,
-) => {}
+    commentId: Types.ObjectId,
+    userId: Types.ObjectId
+) => {
+    const createdReply = await replyModel.create({...data, commentId: commentId, creator: userId});
+    await commentModel.findOneAndUpdate({ _id: commentId }, { $push: { replies: createdReply._id } });
+}
