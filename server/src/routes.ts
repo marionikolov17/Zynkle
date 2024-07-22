80e51697a30924d7bb2d451fc091b7d1170e17a1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:19:32 +0300

    Import comment controller in main app router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userController from "./controllers/user.controller";
 import postController from "./controllers/post.controller";
+import commentController from "./controllers/comment.controller";
 
 const router = express.Router();
 
 router.use("/users", userController);
 router.use("/posts", postController);
+router.use("/comments", commentController);
 
 export default router;
