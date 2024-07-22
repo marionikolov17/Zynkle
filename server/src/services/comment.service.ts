2d46da78d6daea1710fb1acda79c2c8b7bc45d9e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-22 14:59:56 +0300

    Create delete comment method in comment service

--- server/src/services/comment.service.ts
+++ server/src/services/comment.service.ts
@@ -10,3 +10,28 @@ export const createComment = async (
     const createdComment = await commentModel.create({ ...data, postId: postId, creator: userId });
     await postModel.findOneAndUpdate({ _id: postId }, { $push: { comments: createdComment._id } });
 };
+
+export const deleteComment = async (
+    postId: Types.ObjectId,
+    commentId: Types.ObjectId,
+    userId: Types.ObjectId
+) => {
+    if(!(await isCommentOwner(postId, commentId, userId))) {
+        throw new Error("You are unauthorized to delete this comment");
+    }
+
+    await commentModel.findOneAndDelete({ _id: commentId });
+}
+
+const isCommentOwner = async (
+    postId: Types.ObjectId,
+    commentId: Types.ObjectId,
+    userId: Types.ObjectId
+): Promise<boolean> => {
+    const comment = await commentModel.findById(commentId);
+    const post = await postModel.findById(postId);
+
+    if (comment.creator != userId || post.creator !== userId) return false;
+
+    return true;
+}
