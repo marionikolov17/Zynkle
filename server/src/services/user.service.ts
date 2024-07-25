748a72812c34eb87bd2771495d6e283d41b910fb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 13:25:39 +0300

    Populate user posts and saved posts

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -3,7 +3,7 @@ import userModel from "./../models/User";
 import { uploadFileToCloud } from "./../utils/storage-upload";
 
 export const getUser = async (userId: Types.ObjectId) =>
-  userModel.findById(userId, { password: 0 }); // Must populate 
+  userModel.findById(userId, { password: 0 }).populate('posts').populate('savedPosts'); // Must populate 
 
 export const getUsers = async () => {
     return userModel.find();
