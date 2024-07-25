a32329b47ee3332c98fbd4e674a9fde27203aa20
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-25 12:53:55 +0300

    Create like and dislike reply methods in reply service

--- server/src/services/reply.service.ts
+++ server/src/services/reply.service.ts
@@ -4,49 +4,97 @@ import commentModel from "./../models/Comment";
 import postModel from "./../models/Post";
 
 export const createReply = async (
-    data: Record<string, any>,
-    postId: Types.ObjectId,
-    commentId: Types.ObjectId,
-    userId: Types.ObjectId
+  data: Record<string, any>,
+  postId: Types.ObjectId,
+  commentId: Types.ObjectId,
+  userId: Types.ObjectId
 ) => {
-    const createdReply = await replyModel.create({...data, postId: postId, commentId: commentId, creator: userId});
-    await commentModel.findOneAndUpdate({ _id: commentId }, { $push: { replies: createdReply._id } });
-}
+  const createdReply = await replyModel.create({
+    ...data,
+    postId: postId,
+    commentId: commentId,
+    creator: userId,
+  });
+  await commentModel.findOneAndUpdate(
+    { _id: commentId },
+    { $push: { replies: createdReply._id } }
+  );
+};
 
 export const deleteReply = async (
-    postId: Types.ObjectId,
-    replyId: Types.ObjectId,
-    userId: Types.ObjectId
+  postId: Types.ObjectId,
+  replyId: Types.ObjectId,
+  userId: Types.ObjectId
 ) => {
-    if(!(await isReplyOwner(postId, replyId, userId))) {
-        throw new Error("You are unauthorized to delete this reply");
-    }
+  if (!(await isReplyOwner(postId, replyId, userId))) {
+    throw new Error("You are unauthorized to delete this reply");
+  }
 
-    await replyModel.findByIdAndDelete(replyId);
-    // Delete comment relation
-    await commentModel.findOneAndUpdate({ replies: replyId }, { $pull: { replies: replyId } });
-}
+  await replyModel.findByIdAndDelete(replyId);
+  // Delete comment relation
+  await commentModel.findOneAndUpdate(
+    { replies: replyId },
+    { $pull: { replies: replyId } }
+  );
+};
+
+export const likeReply = async (
+  replyId: Types.ObjectId,
+  userId: Types.ObjectId
+) => {
+  if (await hasLikedReply(replyId, userId)) {
+    throw new Error("You have already liked this reply");
+  }
+
+  await replyModel.findByIdAndUpdate(replyId, {
+    $push: { likedBy: userId },
+  });
+};
+
+export const dislikeReply = async (
+  replyId: Types.ObjectId,
+  userId: Types.ObjectId
+) => {
+  if (!(await hasLikedReply(replyId, userId))) {
+    throw new Error("You haven't liked this reply");
+  }
+
+  await replyModel.findByIdAndUpdate(replyId, {
+    $pull: { likedBy: userId },
+  });
+};
 
 const isReplyOwner = async (
-    postId: Types.ObjectId,
-    replyId: Types.ObjectId,
-    userId: Types.ObjectId
+  postId: Types.ObjectId,
+  replyId: Types.ObjectId,
+  userId: Types.ObjectId
 ): Promise<boolean> => {
-    const reply = await replyModel.findById(replyId);
-    const post = await postModel.findById(postId);
+  const reply = await replyModel.findById(replyId);
+  const post = await postModel.findById(postId);
 
-    console.log(reply.creator, userId)
-    if (reply.creator != userId || post.creator != userId) return false;
+  console.log(reply.creator, userId);
+  if (reply.creator != userId || post.creator != userId) return false;
 
-    return true;
-}
+  return true;
+};
+
+const hasLikedReply = async (
+  replyId: Types.ObjectId,
+  userId: Types.ObjectId
+): Promise<boolean> => {
+  const reply = await replyModel.findById(replyId);
+
+  if (reply.likedBy.includes(userId as any)) return true;
+
+  return false;
+};
 
 export const checkIfReplyExsists = async (
-    replyId: Types.ObjectId
+  replyId: Types.ObjectId
 ): Promise<boolean> => {
-    const reply = await replyModel.findById(replyId);
+  const reply = await replyModel.findById(replyId);
 
-    if (!reply) return false;
+  if (!reply) return false;
 
-    return true;
-}
+  return true;
+};
