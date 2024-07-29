678f629d5c8500d0c6033b56c610d4c86d966d9f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-30 00:51:10 +0300

    Add necessary http methods in rest api service

--- client/src/services/rest-api.service.js
+++ client/src/services/rest-api.service.js
@@ -6,3 +6,21 @@ const axiosInstance = axios.create({
     baseURL: BASE_URL
 });
 
+//TODO: Add authentication interceptor
+
+export const get = async (url, config = null) => {
+    return axiosInstance.get(url, config);
+}
+
+export const post = async (url, data, config = null) => {
+    return axiosInstance.post(url, data, config);
+}
+
+export const put = async (url, data, config = null) => {
+    return axiosInstance.put(url, data, config);
+}
+
+export const del = async (url, config = null) => {
+    return axiosInstance.delete(url, config);
+}
+
