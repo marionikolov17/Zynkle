6aff0dfb38029202066dec2cd791fcdbe3d60560
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-30 13:29:16 +0300

    Handle successful login logic

--- client/src/entities/users/hooks/useLogin.js
+++ client/src/entities/users/hooks/useLogin.js
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import * as userService from "./../services/user.service";
 
 export default function useLogin() {
+    const navigate = useNavigate();
+
     const login = async (data) => {
         try {
             const result = await userService.login(data);
 
             console.log(result);
+
+            // Set tokens to local storage
+            localStorage.setItem("accessToken", result.data.data.accessToken);
+            localStorage.setItem("refreshToken", result.data.data.refreshToken);
+            navigate("/");
         } catch (error) {
             throw new Error(error.response.data.data.message);
         }
