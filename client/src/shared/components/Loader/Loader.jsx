182bdfdd2638cb246ad359b9002cb57fe49b56e8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-30 13:23:40 +0300

    Create loader

---
+++ client/src/shared/components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="fixed z-50 w-full min-h-full loader-background flex justify-center items-center">
+            <div className="loader"></div>
+        </div>
+    )
+}
