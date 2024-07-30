569c6fbd9d190714f6c1347b60c7a309f61d3313
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-30 17:14:12 +0300

    Update useAuthenticate.js

--- client/src/entities/users/hooks/useAuthenticate.js
+++ client/src/entities/users/hooks/useAuthenticate.js
@@ -4,19 +4,21 @@ import { useDispatch } from "react-redux";
 import { authenticate } from "../reducers/usersSlice";
 
 import * as userService from "./../services/user.service";
+import { useNavigate } from "react-router-dom";
 
 export default function useAuthenticate() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         (async () => {
             try {
                 const response = await userService.getCurrentUser();
 
-                console.log(response.data.data);
                 dispatch(authenticate(response.data.data));
             } catch (error) {
                 console.log(error);
+                navigate("/login");
             }
         })();
     });
