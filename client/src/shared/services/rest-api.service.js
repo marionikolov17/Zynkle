26593fbe1d9ddc3ccfcd95de9596e978a05bae35
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-07-30 12:50:15 +0300

    Create interceptor for handling network errors

--- client/src/shared/services/rest-api.service.js
+++ client/src/shared/services/rest-api.service.js
@@ -1,26 +1,36 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
-const BASE_URL = 'http://localhost:3000/api/v1/';
+const BASE_URL = "http://localhost:3000/api/v1/";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
+
+// Handle network and connection error
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error instanceof CanceledError || error.message === "Network Error") {
+      alert("server is not working!")
+      // Show maintenance page
+    }
+  }
+);
 
 //TODO: Add authentication interceptor
 
 export const get = async (url, config = null) => {
-    return axiosInstance.get(url, config);
-}
+  return axiosInstance.get(url, config);
+};
 
 export const post = async (url, data, config = null) => {
-    return axiosInstance.post(url, data, config);
-}
+  return axiosInstance.post(url, data, config);
+};
 
 export const put = async (url, data, config = null) => {
-    return axiosInstance.put(url, data, config);
-}
+  return axiosInstance.put(url, data, config);
+};
 
 export const del = async (url, config = null) => {
-    return axiosInstance.delete(url, config);
-}
-
+  return axiosInstance.delete(url, config);
+};
