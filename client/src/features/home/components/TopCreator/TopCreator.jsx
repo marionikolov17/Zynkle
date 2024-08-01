0c2e39395c58d7de6ce9be1b251785ef8ffce8e7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 15:52:20 +0300

    Fetch top creators

--- client/src/features/home/components/TopCreator/TopCreator.jsx
+++ client/src/features/home/components/TopCreator/TopCreator.jsx
@@ -1,18 +1,41 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfilePicture from "../../../../shared/components/ProfilePicture/ProfilePicture";
 
-export default function TopCreator() {
+export default function TopCreator({ creator }) {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className="px-6 my-5 flex justify-around">
       <div className="flex grow justify-start">
-        <ProfilePicture className="w-12 h-12" />
+        <ProfilePicture
+          className="w-12 h-12"
+          imageUrl={creator?.profilePicture}
+        />
         <div className="block ms-2">
-            <Link className="my-auto text-sm font-bold">marionikolov17</Link>
-            <p className="my-auto text-sm opacity-60">Mario Nikolov</p>
+          <Link className="my-auto text-sm font-bold">{creator?.username}</Link>
+          <p className="my-auto text-sm opacity-60">
+            {creator?.firstName + " " + creator?.lastName}
+          </p>
         </div>
       </div>
       <div className="flex shrink justify-end items-center pe-4">
-        <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex">Follow</button>
+        {creator?.followers?.includes(user._id) ? (
+          <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex">
+            Unfollow
+          </button>
+        ) : (
+          <>
+            {creator._id != user._id ? (
+              <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex">
+                Follow
+              </button>
+            ) : (
+              <button className="text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex bg-slate-400">Edit</button>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
--- client/src/features/home/components/TopCreators/TopCreators.jsx
+++ client/src/features/home/components/TopCreators/TopCreators.jsx
@@ -1,14 +1,24 @@
+import useGetTopCreators from "../../../../entities/users/hooks/useGetTopCreators";
+import ErrorToast from "../../../../shared/components/ErrorToast/ErrorToast";
 import TopCreator from "../TopCreator/TopCreator";
 
 export default function TopCreators() {
+  const {creators, error, isLoading} = useGetTopCreators();
+
   return (
     <>
+      {error && <ErrorToast text="Could not get top creators"/>}
+      {isLoading &&
+      <div className="w-full flex justify-center mt-10">
+        <div className="loader"></div>
+      </div>
+      }
       <h3 className="md:hidden lg:block ms-6 font-bold text-lg mb-2">
         Top Creators
       </h3>
+      {creators?.length == 0 && <p className="ms-6 mt-1 text-sm">There are no top creators.</p> }
       {/* <p className="ms-6 mt-1 text-sm">There are no top creators.</p> */}
-      <TopCreator />
-      <TopCreator />
+      {creators?.map(creator => <TopCreator key={creator?._id} creator={creator}/>)}
     </>
   );
 }
