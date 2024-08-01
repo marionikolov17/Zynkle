3c7ade92e1bf6510c6c3b01948b9a3cd5076cebe
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 21:40:15 +0300

    Create useSearchUser.js

---
+++ client/src/entities/users/hooks/useSearchUser.js
@@ -0,0 +1,16 @@
+import * as userService from "./../services/user.service";
+
+export default function useSearchUser() {
+    const search = async (query, setUsers) => {
+        try {
+            const response = await userService.searchUser(query);
+
+            setUsers(response.data.data.users);
+        } catch (error) {
+            console.log(error)
+            throw new Error(error.message);
+        } 
+    }
+
+    return search;
+}
