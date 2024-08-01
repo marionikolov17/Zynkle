7bee84015207b3d0079416152f0d2a6d5eeeb4de
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 16:10:14 +0300

    Implement top creators follow and unfollow

--- client/src/entities/users/hooks/useGetTopCreators.js
+++ client/src/entities/users/hooks/useGetTopCreators.js
@@ -2,25 +2,25 @@ import { useEffect, useState } from "react";
 import * as userService from "./../services/user.service";
 
 export default function useGetTopCreators() {
-    const [creators, setCreators] = useState();
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [creators, setCreators] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setIsLoading(true);
-        (async () => {
-            try {
-                const response = await userService.getTopCreators();
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        const response = await userService.getTopCreators();
 
-                console.log(response.data.data.users);
-                setCreators(response.data.data.users)
-            } catch (error) {
-                setError(error);
-            } finally {
-                setIsLoading(false);
-            }
-        })()
-    }, []);
+        console.log(response.data.data.users);
+        setCreators(response.data.data.users);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
 
-    return { creators, error, isLoading };
+  return { creators, error, isLoading };
 }
--- client/src/features/home/components/TopCreator/TopCreator.jsx
+++ client/src/features/home/components/TopCreator/TopCreator.jsx
@@ -2,41 +2,76 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfilePicture from "../../../../shared/components/ProfilePicture/ProfilePicture";
+import { useFollowProfile, useUnfollowProfile } from "../../../../entities/users/hooks/useProfile";
+import { useState } from "react";
+import ErrorToast from "../../../../shared/components/ErrorToast/ErrorToast";
 
 export default function TopCreator({ creator }) {
+  const [error, setError] = useState(null);
+  const [followers, setFollowers] = useState(creator?.followers || []);
+
   const user = useSelector((state) => state.user);
 
+  const follow = useFollowProfile();
+  const unfollow = useUnfollowProfile();
+
+  const onFollow = async () => {
+    try {
+      await follow(creator?._id);
+      setFollowers(currentFollowers => {
+        return [...currentFollowers, user._id];
+      })
+    } catch (error) {
+      setError("An error occured")
+    } 
+  }
+
+  const onUnfollow = async () => {
+    try {
+      await unfollow(creator._id);
+      setFollowers(currentFollowers => {
+        let newFollowers = currentFollowers.filter(id => id != user._id);
+        return newFollowers;
+      })
+    } catch (error) {
+      setError("An error occured")
+    } 
+  }
+
   return (
-    <div className="px-6 my-5 flex justify-around">
-      <div className="flex grow justify-start">
-        <ProfilePicture
-          className="w-12 h-12"
-          imageUrl={creator?.profilePicture}
-        />
-        <div className="block ms-2">
-          <Link className="my-auto text-sm font-bold">{creator?.username}</Link>
-          <p className="my-auto text-sm opacity-60">
-            {creator?.firstName + " " + creator?.lastName}
-          </p>
+    <>
+      {error && <ErrorToast text={error}/>}
+      <div className="px-6 my-5 flex justify-around">
+        <div className="flex grow justify-start">
+          <ProfilePicture
+            className="w-12 h-12"
+            imageUrl={creator?.profilePicture}
+          />
+          <div className="block ms-2">
+            <Link className="my-auto text-sm font-bold">{creator?.username}</Link>
+            <p className="my-auto text-sm opacity-60">
+              {creator?.firstName + " " + creator?.lastName}
+            </p>
+          </div>
+        </div>
+        <div className="flex shrink justify-end items-center pe-4">
+          {followers?.includes(user._id) ? (
+            <button className="border border-mainGreen text-mainGreen rounded-lg py-2 px-8 h-max hidden 2xl:flex" onClick={onUnfollow}>
+              Unfollow
+            </button>
+          ) : (
+            <>
+              {creator._id != user._id ? (
+                <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex" onClick={onFollow}>
+                  Follow
+                </button>
+              ) : (
+                <Link to="/profile/edit" className="rounded-lg py-2 px-8 h-max hidden 2xl:flex border border-black">Edit</Link>
+              )}
+            </>
+          )}
         </div>
       </div>
-      <div className="flex shrink justify-end items-center pe-4">
-        {creator?.followers?.includes(user._id) ? (
-          <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex">
-            Unfollow
-          </button>
-        ) : (
-          <>
-            {creator._id != user._id ? (
-              <button className="bg-mainGreen text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex">
-                Follow
-              </button>
-            ) : (
-              <Link to="/profile/edit" className="text-white rounded-lg py-2 px-8 h-max hidden 2xl:flex bg-slate-400">Edit</Link>
-            )}
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
