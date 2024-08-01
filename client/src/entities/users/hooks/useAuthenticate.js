fbb433834bc00813e9b83d5cff0c63693e1fa9f4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 23:31:43 +0300

    Update useAuthenticate.js

--- client/src/entities/users/hooks/useAuthenticate.js
+++ client/src/entities/users/hooks/useAuthenticate.js
@@ -23,7 +23,7 @@ export default function useAuthenticate() {
 
         dispatch(authenticate(response.data.data));
       } catch (error) {
-        console.log(error);
+        //console.log(error);
         if (publicPathnames.includes(location.pathname)) return;
         navigate("/login");
       }
