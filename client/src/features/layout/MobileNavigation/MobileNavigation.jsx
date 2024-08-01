292e546748fd84e149280392dccfa91f53ad58ac
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 16:20:28 +0300

    Update MobileNavigation.jsx

--- client/src/features/layout/MobileNavigation/MobileNavigation.jsx
+++ client/src/features/layout/MobileNavigation/MobileNavigation.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { CiHome, CiSearch, CiCirclePlus } from "react-icons/ci";
 import ProfilePicture from "./../../../shared/components/ProfilePicture/ProfilePicture";
+import { useSelector } from "react-redux";
 
 export default function MobileNavigation() {
+  const user = useSelector(state => state.user);
+
   return (
     <div className="flex items-center justify-around w-full fixed bottom-0 bg-mainWhite border-t-2 h-10 z-40 sm:hidden">
       <Link to="/">
@@ -14,7 +17,7 @@ export default function MobileNavigation() {
       <Link to="/create">
         <CiCirclePlus className="text-2xl my-auto" />
       </Link>
-      <ProfilePicture className="w-6 h-6" />
+      <ProfilePicture imageUrl={user?.profilePicture} className="w-6 h-6" />
     </div>
   );
 }
