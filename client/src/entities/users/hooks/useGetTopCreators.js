1d931d6404ba6bd6109103c9c235f4a14fa08122
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 15:42:01 +0300

    Create useGetTopCreators.js

---
+++ client/src/entities/users/hooks/useGetTopCreators.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import * as userService from "./../services/user.service";
+
+export default function useGetTopCreators() {
+    const [creators, setCreators] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        (async () => {
+            try {
+                const response = await userService.getTopCreators();
+
+                console.log(response.data.data.users);
+                setCreators(response.data.data.users)
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        })()
+    }, []);
+
+    return { creators, error, isLoading };
+}
