43a76809eab9654a1622d19bc823f9aa229373c2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-01 20:42:24 +0300

    Create useUpdateProfile.js

---
+++ client/src/entities/users/hooks/useUpdateProfile.js
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import * as userService from "./../services/user.service";
+
+export default function useUpdateProfile() {
+    const navigate = useNavigate();
+
+    const updateUser = async (data) => {
+        try {
+            await userService.updateUser(data);
+
+            navigate("/profile");
+        } catch (error) {
+            throw new Error(error.response.data.data.message);
+        }
+    }
+
+    return updateUser;
+}
