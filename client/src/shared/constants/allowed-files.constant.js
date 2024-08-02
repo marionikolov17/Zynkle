c1d0a62893386a6002eabf3c73768d7b7d35049b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-02 17:21:50 +0300

    Create allowed-files.constant.js

---
+++ client/src/shared/constants/allowed-files.constant.js
@@ -0,0 +1,10 @@
+export const allowedImageMimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/bmp',
+    'image/tiff',
+    'image/webp',
+    'image/heif',
+    'image/heic',
+]
