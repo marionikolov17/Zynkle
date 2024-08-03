122e9b183c7e7a71ce37455496b8a75ae4d23ce4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 11:32:38 +0300

    Create usePost.js

---
+++ client/src/entities/posts/hooks/usePost.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import * as postService from "./../services/post.service";
+
+export const useGetPost = (postId) => {
+    const [post, setPost] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        (async () => {
+            try {
+                const response = await postService.getPost(postId);
+
+                setPost(response.data.data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        })
+    }, [postId]);
+
+    return { post, loading, error }
+}
