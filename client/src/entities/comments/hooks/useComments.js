6f624329dadbcf1c99d558ddae079093ae2d94fe
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 12:54:54 +0300

    Create useGetPostComments hook

---
+++ client/src/entities/comments/hooks/useComments.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import * as commentService from "./../services/comment.service";
+
+export const useGetPostComments = (postId) => {
+    const [comments, setComments] = useState();
+    const [commentsLoading, setCommentsLoading] = useState(false);
+    const [commentsError, setCommentsError] = useState();
+
+    useEffect(() => {
+        setCommentsLoading(true);
+        (async () => {
+            try {
+                const response = await commentService.getPostComments(postId);
+
+                setComments(response.data.data);
+            } catch (error) {
+                setCommentsError(error);
+            } finally {
+                setCommentsLoading(false);
+            }
+        })();
+    }, [postId])
+
+    return { comments, commentsLoading, commentsError }
+}
