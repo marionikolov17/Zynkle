c37762188f5b056a77db8e69c7ade3745260811c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 11:28:05 +0300

    Create get post method in FE service

--- client/src/entities/posts/services/post.service.js
+++ client/src/entities/posts/services/post.service.js
@@ -2,3 +2,5 @@ import * as apiService from "./../../../shared/services/rest-api.service";
 import PATHS from "../../../shared/configs/paths.config";
 
 export const createPost = async (data) => apiService.post(PATHS.POSTS, data);
+
+export const getPost = async (postId) => apiService.get(PATHS.POSTS + postId);
