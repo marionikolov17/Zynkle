1bae6d84fb5bfb8cf5d5a12bd3accaf6acb4af03
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 18:46:39 +0300

    Implement like and dislike reply methods in FE service

--- client/src/entities/replies/services/reply.service.ts
+++ client/src/entities/replies/services/reply.service.ts
@@ -2,3 +2,7 @@ import PATHS from "../../../shared/configs/paths.config";
 import * as apiService from "./../../../shared/services/rest-api.service";
 
 export const getCommentReplies = async (commentId) => apiService.get(PATHS.REPLIES + commentId);
+
+export const likeReply = async (replyId) => apiService.put(PATHS.REPLIES + replyId + "/like");
+
+export const dislikeReply = async (replyId) => apiService.put(PATHS.REPLIES + replyId + "/dislike");
