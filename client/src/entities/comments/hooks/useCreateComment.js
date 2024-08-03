c6fa1369a4618703ec187c8ed690f046b3be042f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 13:31:37 +0300

    Update useCreateComment.js

--- client/src/entities/comments/hooks/useCreateComment.js
+++ client/src/entities/comments/hooks/useCreateComment.js
@@ -4,6 +4,8 @@ export default function useCreateComment() {
     const createComment = async (postId, data) => {
         try {
             const response = await commentService.createComment(postId, data);
+
+            return response.data.data;
         } catch (error) {
             throw new Error(error.response.data.data.message);
         }
