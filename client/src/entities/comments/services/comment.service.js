38866cfbfb120e9e946e4063cca91edb8f037fa0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-03 13:24:32 +0300

    Implement Create post comment method in FE service

--- client/src/entities/comments/services/comment.service.js
+++ client/src/entities/comments/services/comment.service.js
@@ -1,4 +1,6 @@
 import PATHS from "../../../shared/configs/paths.config";
 import * as apiService from "./../../../shared/services/rest-api.service";
 
+export const createComment = async (postId, data) => apiService.post(PATHS.COMMENTS + postId, data);
+
 export const getPostComments = async (postId) => apiService.get(PATHS.COMMENTS + postId);
