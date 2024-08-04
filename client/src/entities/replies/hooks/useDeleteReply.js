2cf26783c60b19565375101a9a2b5e13d14671e7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-04 12:15:05 +0300

    Create useDeleteReply hook

--- client/src/entities/replies/hooks/useDeleteReply.js
+++ client/src/entities/replies/hooks/useDeleteReply.js
@@ -1,3 +1,23 @@
+import * as replyService from "./../services/reply.service";
+
 export default function useDeleteReply() {
-    
+    const deleteReply = async (postId, replyId) => {
+        try {
+            await replyService.deleteReply(postId, replyId);
+
+            return;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    const onDeleteReply = (setReplies, replyId) => {
+        setReplies(replies => {
+            const newReplies = replies?.filter(reply => reply?._id != replyId);
+
+            return newReplies;
+        })
+    }
+
+    return { deleteReply, onDeleteReply }
 }
