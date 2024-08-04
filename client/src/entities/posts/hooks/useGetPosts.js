c9f18c3741a2444872c430197ed2c2c9754fe11c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-04 15:40:18 +0300

    Create useGetPosts.js

---
+++ client/src/entities/posts/hooks/useGetPosts.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import * as postService from "./../services/post.service";
+
+export default function useGetPosts(pageNumber) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const response = await postService.getPosts(pageNumber);
+        const posts = response.data.data;
+
+        setPosts((prevPosts) => {
+          return [...prevPosts, ...posts];
+        });
+
+        setHasMore(posts?.length > 0);
+      } catch (error) {
+        setError(error.response.data.data.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [pageNumber]);
+
+  return { posts, loading, error, hasMore };
+}
