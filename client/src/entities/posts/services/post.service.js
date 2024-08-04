e1d8021db4e455f4bf3b31cd21db4cce6aa78f8c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-04 15:34:09 +0300

    Create get posts method in FE service

--- client/src/entities/posts/services/post.service.js
+++ client/src/entities/posts/services/post.service.js
@@ -1,6 +1,8 @@
 import * as apiService from "./../../../shared/services/rest-api.service";
 import PATHS from "../../../shared/configs/paths.config";
 
+export const getPosts = async(pageNumber) => apiService.get(`/posts?page=${pageNumber}`);
+
 export const createPost = async (data) => apiService.post(PATHS.POSTS, data);
 
 export const getPost = async (postId) => apiService.get(PATHS.POSTS + postId);
