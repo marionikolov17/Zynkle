17b630a103937aca49c71bbae4f195c39180bbaf
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-04 16:17:04 +0300

    Update useGetPosts.js

--- client/src/entities/posts/hooks/useGetPosts.js
+++ client/src/entities/posts/hooks/useGetPosts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import * as postService from "./../services/post.service";
 
@@ -27,5 +28,63 @@ export default function useGetPosts(pageNumber) {
     })();
   }, [pageNumber]);
 
-  return { posts, loading, error, setError, hasMore };
+  const onLikePost = (postId, userId) => {
+    setPosts(posts => {
+        const newPosts = posts.map(post => {
+            if (post?._id == postId) {
+                post?.likedBy?.push(userId);
+            }
+
+            return post;
+        });
+
+        return newPosts;
+    })
+  }
+
+  const onDislikePost = (postId, userId) => {
+    setPosts(posts => {
+        const newPosts = posts.map(post => {
+            if (post?._id == postId) {
+                const newLikedBy = post?.likedBy?.filter(id => id != userId);
+                post.likedBy = newLikedBy;
+            }
+
+            return post;
+        });
+
+        return newPosts;
+    })
+  }
+
+  const onSavePost = (postId, userId) => {
+    setPosts(posts => {
+        const newPosts = posts.map(post => {
+            if (post?._id == postId) {
+                post?.savedBy?.push(userId);
+            }
+
+            return post;
+        });
+
+        return newPosts;
+    })
+  }
+
+  const onUnsavePost = (postId, userId) => {
+    setPosts(posts => {
+        const newPosts = posts.map(post => {
+            if (post?._id == postId) {
+                const newSavedBy = post?.savedBy?.filter(id => id != userId);
+                post.savedBy = newSavedBy;
+            }
+
+            return post;
+        });
+
+        return newPosts;
+    })
+  }
+
+  return { posts, loading, error, setError, hasMore, onLikePost, onDislikePost, onSavePost, onUnsavePost };
 }
