e1286ef307aa90bfb2d36a46eca32bc4329b8142
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-05 14:00:36 +0300

    Test cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,13 +19,25 @@ import {
 import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
-app.use(cors());
-app.use(
-  cors({
-    origin: "https://zynkle-frontend.vercel.app/",
-    credentials: true,
-  })
-);
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
+  next();
+});
 
 expressConfig(app);
 
