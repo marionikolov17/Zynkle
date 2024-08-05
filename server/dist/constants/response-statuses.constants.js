b4cff3a3b0121f79f869d553f8d874ae86aae291
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-05 14:13:04 +0300

    Test commit

--- server/dist/constants/response-statuses.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RESPONSE_STATUS = {
-    FAILED: "fail",
-    SUCCESS: "success",
-    ERROR: "error"
-};
-exports.default = RESPONSE_STATUS;
-//# sourceMappingURL=response-statuses.constants.js.map
--- server/dist/services/invalid-tokens.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.invalidateAccessToken = exports.invalidAccessTokens = void 0;
-exports.invalidAccessTokens = [];
-const invalidateAccessToken = (token) => exports.invalidAccessTokens.push(token);
-exports.invalidateAccessToken = invalidateAccessToken;
-//# sourceMappingURL=invalid-tokens.js.map
