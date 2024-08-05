277fc3577b2ab2669817c106a8a205598cd36136
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-05 13:47:49 +0300

    Change FE base_url to production server url

--- client/src/shared/services/rest-api.service.js
+++ client/src/shared/services/rest-api.service.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import axios, { CanceledError } from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://zynkle-server.vercel.app/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
