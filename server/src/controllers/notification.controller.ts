80afd6cf37598846243ee4aab9cb6e69917e0976
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-19 13:23:07 +0300

    Create route for getting user notifications

--- server/src/controllers/notification.controller.ts
+++ server/src/controllers/notification.controller.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import * as notificationService from "./../services/notification.service";
+import { tryCatch } from "../utils/tryCatch";
+import { isAuth } from "../middlewares/auth.middleware";
 
 const router = express.Router();
+
+router.get(
+  "/",
+  isAuth,
+  tryCatch(async (req: any, res: express.Response) => {
+    const notifications = await notificationService.getNotifications(req.user._id);
+
+    res.status(200).json({
+        status: "success",
+        data: notifications
+    })
+  })
+);
 
 export default router;
