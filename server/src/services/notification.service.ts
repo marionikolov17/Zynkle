62664b3d7d8fc4b80609d89d5fc1f55978613475
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-19 12:25:03 +0300

    Create notification.service.ts

---
+++ server/src/services/notification.service.ts
@@ -0,0 +1,15 @@
+export const getNotifications = async () => {
+
+}
+
+export const createNotification = async () => {
+
+}
+
+export const readNotifications = async () => {
+
+}
+
+export const checkForNotifications = async () => {
+    
+}
